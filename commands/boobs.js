9bdcccc336a01220ae70e330b44bb06b82524e80
Soba <73451052+sobasoskuks@users.noreply.github.com>
2020-11-10 07:57:22 +0700

    Create boobs.js

---
+++ commands/boobs.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const axios = require('axios');
+const { MessageEmbed } = require("discord.js");
+module.exports.config = { 
+    name: "boobs",
+    aliases: ['boobs']
+}
+
+module.exports.run = async (client, message, args) =>  {
+
+if(!message.channel.nsfw) return message.channel.send("😾 | This is not NSFW channels!")
+ 
+    let randomNum = Math.floor(Math.random() * 25);
+    await axios.get(`https://www.reddit.com/r/boobs.json`).then(r => {
+        message.channel.send(
+            
+            new MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle(r.data.data.children[randomNum].data.title)
+            .setAuthor(`Posted by: ${r.data.data.children[randomNum].data.author}`)
+            .setImage(r.data.data.children[randomNum].data.url)
+            );
+    }).catch (console.error());
+} 
+ 
